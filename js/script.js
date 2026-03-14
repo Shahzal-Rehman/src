874a594c69303e7dcbd58d99ab172c9b16a9e07e
Shahzal Rehman <shahzalrehman0302@gmail.com>
2026-03-14 14:53:37 +0500

    Update script.js
    Signed-off-by: Shahzal Rehman <shahzalrehman0302@gmail.com>

--- js/script.js
+++ js/script.js
@@ -2,27 +2,3 @@ document.getElementById("toggle").addEventListener("click", function()
 {
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
-
-
-<script>
-const hamburger = document.getElementById('hamburger');
-const sidebar = document.getElementById('sidebar');
-
-hamburger.addEventListener('click', () => {
-  if (sidebar.style.left === '0px') {
-    sidebar.style.left = '-250px';
-    document.body.classList.remove('sidebar-open');
-  } else {
-    sidebar.style.left = '0px';
-    document.body.classList.add('sidebar-open');
-  }
-});
-
-// Optional: close sidebar if clicked outside
-document.body.addEventListener('click', function(e){
-  if(!sidebar.contains(e.target) && !hamburger.contains(e.target)) {
-    sidebar.style.left = '-250px';
-    document.body.classList.remove('sidebar-open');
-  }
-});
-</script>
